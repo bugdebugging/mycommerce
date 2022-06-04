63d0dbec6094aa6677abb5e9b18629e899f2573b
bugdebugging <annyousung@gmail.com>
2022-06-05 00:52:05 +0900

    test:ShoppingCartItem 저장

---
+++ src/test/java/com/anny/mycommerce/cart/ShoppingCartItemRepositoryTest.java
@@ -0,0 +1,67 @@
+package com.anny.mycommerce.cart;
+
+import com.anny.mycommerce.cart.domain.ShoppingCartItem;
+import com.anny.mycommerce.cart.domain.ShoppingCartItemRepository;
+import com.anny.mycommerce.category.domain.Category;
+import com.anny.mycommerce.category.domain.CategoryRepository;
+import com.anny.mycommerce.product.domain.Product;
+import com.anny.mycommerce.product.domain.ProductRepository;
+import com.anny.mycommerce.store.domain.Store;
+import com.anny.mycommerce.store.domain.StoreRepository;
+import com.anny.mycommerce.user.domain.User;
+import com.anny.mycommerce.user.domain.application.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@Transactional
+@SpringBootTest
+public class ShoppingCartItemRepositoryTest {
+    @Autowired
+    ShoppingCartItemRepository shoppingCartItemRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    StoreRepository storeRepository;
+
+    @Test
+    void 쇼핑카트아이템_저장() {
+        User user = User.forTest().build();
+        Category category = Category.of("가전제품");
+
+        Store store = Store.of()
+                .name("test store")
+                .admin(user)
+                .build();
+
+        Product product = Product.forTest()
+                .store(store)
+                .category(category)
+                .build();
+
+        userRepository.save(user);
+        categoryRepository.save(category);
+        storeRepository.save(store);
+        productRepository.save(product);
+
+        ShoppingCartItem shoppingCartItem = ShoppingCartItem.of()
+                .user(user)
+                .product(product)
+                .count(3)
+                .build();
+
+        ShoppingCartItem savedShoppingCartItem = shoppingCartItemRepository.save(shoppingCartItem);
+        assertNotNull(savedShoppingCartItem.getId());
+    }
+}
