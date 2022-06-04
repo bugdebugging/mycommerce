7a7d1b6b53660d8c0e937a58b3b99abae164a0bd
bugdebugging <annyousung@gmail.com>
2022-06-04 15:12:42 +0900

    test:store 저장 테스트

---
+++ src/test/java/com/anny/mycommerce/store/StoreRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.anny.mycommerce.store;
+
+import com.anny.mycommerce.store.domain.Store;
+import com.anny.mycommerce.store.domain.StoreRepository;
+import com.anny.mycommerce.user.domain.User;
+import com.anny.mycommerce.user.domain.application.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@Transactional
+@SpringBootTest
+public class StoreRepositoryTest {
+    @Autowired
+    private StoreRepository storeRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void 스토어_저장() {
+        final User user=User.forTest().build();
+        final String name = "let's car~";
+        userRepository.save(user);
+
+        Store store = Store.of()
+                .name(name)
+                .admin(user)
+                .build();
+        Store savedStore = storeRepository.save(store);
+        assertNotNull(savedStore.getId());
+    }
+}
