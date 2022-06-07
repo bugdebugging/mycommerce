7337cd1639222b9963cbfb1713b2612e2b0b1479
bugdebugging <annyousung@gmail.com>
2022-06-07 22:00:09 +0900

    fix:Review EqualsAndHashCode, Builder 추가

--- src/main/java/com/anny/mycommerce/review/domain/Review.java
+++ src/main/java/com/anny/mycommerce/review/domain/Review.java
@@ -1,13 +1,10 @@
 package com.anny.mycommerce.review.domain;
 
 import com.anny.mycommerce.common.domain.Image;
-import com.anny.mycommerce.product.domain.Product;
-import com.anny.mycommerce.user.domain.User;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.util.Assert;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -17,6 +14,7 @@ import java.util.List;
 @Entity
 @Table(name = "reviews")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(of = "id")
 public class Review {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -46,7 +44,14 @@ public class Review {
     @UpdateTimestamp
     private LocalDateTime updatedAt;
 
+    @Builder(builderMethodName = "of")
     public Review(Long writerId, Long productId, String content, int rate, List<Image> images) {
+        Assert.notNull(writerId, "리뷰 작성자의 id는 필수입니다.");
+        Assert.notNull(productId, "리뷰 상품의 id는 필수입니다.");
+        Assert.notNull(content, "리뷰의 내용글은 null일 수 없습니다.");
+        Assert.state(rate>=0 && rate<=5,"리뷰 점수는 0~5점만 가능합니다.");
+        Assert.notNull(images,"리뷰의 이미지 리스트는 null일 수 없습니다.");
+
         this.writerId = writerId;
         this.productId = productId;
         this.content = content;
