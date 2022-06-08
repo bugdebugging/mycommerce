9ad02fc7e4130839077816285bb9ab26e63ad790
bugdebugging <annyousung@gmail.com>
2022-06-08 11:38:44 +0900

    feat:CouponGroupRepository

---
+++ src/main/java/com/anny/mycommerce/coupon/domain/CouponGroupRepository.java
@@ -0,0 +1,6 @@
+package com.anny.mycommerce.coupon.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CouponGroupRepository extends JpaRepository<CouponGroup, Long> {
+}
